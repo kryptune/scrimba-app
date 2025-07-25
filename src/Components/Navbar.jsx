f9aaa4c7aa4620de7f4e9397f65e9ed6e796418f
kryptune <51198180+kryptune@users.noreply.github.com>
2025-07-25 18:30:56 +0800

    first upload

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function Navbar() {
+    return (
+        <nav id="NavBar">
+            <img id="icon" src="/icon.png" />
+            <h3>ReactFacts</h3>
+            <h4>React Course-Project 1</h4>
+        </nav> 
+    )
+}
